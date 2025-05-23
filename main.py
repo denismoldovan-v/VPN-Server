9c7aa68a59115644077f26010b2cf8a8146d1b0a
denismoldovan-v <iamdenismoldovan@gmail.com>
2025-05-23 11:55:59 +0200

    Added basic SOCKS5 proxy server for TCP tunneling

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ This script initializes all major components:
 from logger import setup_logger
 from crypto_utils import load_private_key, load_public_key
 from tun_interface import create_tun_interface, configure_interface
+from socks5_proxy import start_socks5_proxy
+
 
 logger = setup_logger(config["log_file"])
 logger.info("VPN server started.")
@@ -20,3 +22,5 @@ public_key = load_public_key(config["public_key_path"])
 
 tun_fd = create_tun_interface(config["tun_interface"])
 configure_interface(config["tun_interface"], config["tun_ip"], config["tun_netmask"])
+
+threading.Thread(target=start_socks5_proxy, args=(config["socks5_port"],), daemon=True).start()
--- socks5_proxy.py
+++ socks5_proxy.py
@@ -6,3 +6,57 @@ Receives client connections and:
 - Establishes TCP connections through the VPN tunnel
 - Forwards traffic between client and target servers
 """
+
+import socket
+import threading
+import struct
+
+def handle_client(client_socket):
+    # SOCKS5 handshake (version, methods)
+    client_socket.recv(2)         # Version + nmethods
+    client_socket.recv(1)         # Methods (we ignore and accept all)
+    client_socket.sendall(b"\x05\x00")  # Accept "no auth"
+
+    # Request header
+    ver, cmd, _, atype = struct.unpack("!BBBB", client_socket.recv(4))
+    
+    if atype == 1:  # IPv4
+        addr = socket.inet_ntoa(client_socket.recv(4))
+    else:
+        client_socket.close()
+        return
+
+    port = struct.unpack('!H', client_socket.recv(2))[0]
+
+    # Connect to destination
+    remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        remote.connect((addr, port))
+        client_socket.sendall(b"\x05\x00\x00\x01" + socket.inet_aton("0.0.0.0") + struct.pack("!H", 0))
+    except:
+        client_socket.close()
+        return
+
+    # Relay
+    def relay(src, dst):
+        while True:
+            try:
+                data = src.recv(4096)
+                if not data:
+                    break
+                dst.sendall(data)
+            except:
+                break
+
+    threading.Thread(target=relay, args=(client_socket, remote)).start()
+    threading.Thread(target=relay, args=(remote, client_socket)).start()
+
+def start_socks5_proxy(port: int):
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(("0.0.0.0", port))
+    server.listen(5)
+    print(f"[SOCKS5] Listening on port {port}")
+
+    while True:
+        client, _ = server.accept()
+        threading.Thread(target=handle_client, args=(client,)).start()
