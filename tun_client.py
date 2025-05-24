0fcbb74b0f0682ad71933ab37e0920a09d7864ca
denismoldovan-v <iamdenismoldovan@gmail.com>
2025-05-24 23:55:19 +0200

    Add TLS support to VPN client using SSL socket wrapping

--- tun_client.py
+++ tun_client.py
@@ -1,4 +1,5 @@
 ﻿import socket
+import ssl
 import threading
 import os
 import time
@@ -9,7 +10,7 @@ from logger import setup_logger
 
 logger = setup_logger("vpn.log")
 
-SERVER_IP = "91.99.126.179"  
+SERVER_IP = "91.99.126.179"
 SERVER_PORT = 5555
 
 def forward_tun_to_socket(tun_fd, sock):
@@ -25,34 +26,34 @@ def forward_socket_to_tun(sock, tun_fd):
         os.write(tun_fd, packet)
 
 def authenticate_with_server(sock):
-    private_key = load_private_key("keys/private.pem")  # Twój klucz prywatny klienta
+    private_key = load_private_key("keys/private.pem")
 
-    # Odbierz nonce od serwera
     nonce = sock.recv(32)
-    # Podpisz nonce
     signature = sign_with_private_key(nonce, private_key)
-    # Wyślij podpis z powrotem
     sock.sendall(signature)
 
 def main():
-    # Łączymy się z serwerem
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((SERVER_IP, SERVER_PORT))
+    try:
+        raw_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        context = ssl.create_default_context()
+        sock = context.wrap_socket(raw_sock, server_hostname=SERVER_IP)
+        sock.connect((SERVER_IP, SERVER_PORT))
+        logger.info("[VPN CLIENT] Connected to server with TLS")
+    except Exception as e:
+        logger.error(f"[VPN CLIENT] TLS connection failed: {e}")
+        return
 
-    # Uwierzytelnianie
     authenticate_with_server(sock)
 
     client_ip = socket.inet_ntoa(sock.recv(4))
     server_ip = socket.inet_ntoa(sock.recv(4))
 
     tun_name = f"tun{os.getpid() % 1000}{int(time.time()) % 1000}"
-
     tun_fd = create_tun_interface(tun_name)
     configure_interface(tun_name, client_ip, "255.255.255.0")
 
     logger.info(f"[VPN CLIENT] Configured TUN interface: {tun_name} with IP {client_ip}, server peer: {server_ip}")
 
-    # Przepychamy pakiety
     threading.Thread(target=forward_tun_to_socket, args=(tun_fd, sock)).start()
     threading.Thread(target=forward_socket_to_tun, args=(sock, tun_fd)).start()
 
