6b6272666d5964f0f313c1535404978f2bd15abf
denismoldovan-v <iamdenismoldovan@gmail.com>
2025-05-24 12:19:45 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-VENV_DIR="$HOME/vpn-venv"
+VENV_DIR="./vpn-venv"  
 
-# Tworzenie środowiska virtualenv jeśli nie istnieje
 if [ ! -d "$VENV_DIR" ]; then
     echo "Tworzenie środowiska venv w $VENV_DIR"
     python3 -m venv "$VENV_DIR"
 fi
 
-# Instalacja zależności bez aktywacji venv (bo i tak uruchamiamy z PATH)
 if [ -f requirements.txt ]; then
     echo "Instalacja pakietów z requirements.txt"
-    "$VENV_DIR/bin/pip" install --upgrade pip >/dev/null
+    "$VENV_DIR/bin/pip" install --upgrade pip
     "$VENV_DIR/bin/pip" install -r requirements.txt
 else
     echo "Brak pliku requirements.txt"
     exit 1
 fi
 
-# Uruchomienie aplikacji z uprawnieniami root i z venv w PATH
+
 echo "Uruchamianie serwera VPN..."
-sudo env "PATH=$VENV_DIR/bin:$PATH" python main.py
+sudo env "PATH=$(realpath $VENV_DIR)/bin:$PATH" python main.py
