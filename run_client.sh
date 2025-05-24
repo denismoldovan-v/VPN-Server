d3696a9056ae692801549f16363c379070f3b524
denismoldovan-v <iamdenismoldovan@gmail.com>
2025-05-24 12:31:49 +0200

    Added run_client.sh

---
+++ run_client.sh
@@ -0,0 +1,14 @@
+﻿#!/bin/bash
+
+VENV_DIR="./vpn-venv"
+
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Tworzenie środowiska venv w $VENV_DIR"
+    python3 -m venv "$VENV_DIR"
+fi
+
+"$VENV_DIR/bin/pip" install --upgrade pip
+"$VENV_DIR/bin/pip" install -r requirements.txt
+
+echo "Uruchamianie klienta VPN..."
+sudo env "PATH=$(realpath $VENV_DIR)/bin:$PATH" python3 tun_client.py
